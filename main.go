2fc415b8ac67a194707924e05887edf41b9fa960
schwamster <bastian.toepfer@gmail.com>
2017-10-16 15:14:52 +0200

    more personal hw msg

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 		delayNum, _ := strconv.Atoi(delay)
 		sleep(time.Duration(delayNum) * time.Millisecond)
 	}
-	io.WriteString(w, "hello, world!\n")
+	io.WriteString(w, "hello, Bastian!\n")
 }
 
 func RandomErrorServer(w http.ResponseWriter, req *http.Request) {
